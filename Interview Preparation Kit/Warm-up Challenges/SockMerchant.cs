3af6c8ee4de5e1fc6a2b239b45d2d55ff680b61e
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-11 19:13:07 -0300

    Resolve SockMerchant for Interview Preparation

---
+++ Interview Preparation Kit/Warm-up Challenges/SockMerchant.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        var pairsOfSocks = 0;
+
+        var socksList = ar.ToList();
+        socksList.Sort();
+        
+        for(var i = 1; i < n; i++){
+            if(socksList[i] == socksList[i-1]){
+                pairsOfSocks += 1;
+                i += 1;
+            }
+           
+        }
+
+        return pairsOfSocks;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
