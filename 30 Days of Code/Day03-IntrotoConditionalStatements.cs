8f2e6727565fa297cbdad117003f8fb783b906ac
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-12 13:44:17 -0300

    Resolve Day03-IntroConditionalStatements challange

---
+++ 30 Days of Code/Day03-IntrotoConditionalStatements.cs
@@ -0,0 +1,23 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    static void Main(string[] args) {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        if((N % 2 == 1) || (N >= 6 && N <= 20)) Console.WriteLine("Weird");
+        else if((N >= 2 && N <= 5) || N > 20) Console.WriteLine("Not Weird");
+    }
+}
