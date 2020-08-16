fae3fafda80e63055e1135904e9a7b92150515c8
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-16 15:03:51 -0300

    Resolve Day07-Arrays challange

---
+++ 30 Days of Code/Day07-Arrays.cs
@@ -0,0 +1,32 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    public static string arrayReverseToString(int[] arr){
+        string arrString = "";
+        foreach(int itemArr in arr.Reverse()){
+            arrString += $"{itemArr} ";
+        }
+        return arrString;
+    }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        
+        Console.WriteLine(arrayReverseToString(arr));
+    }
+}
