c883db8e612b1da472479c5d438fc5b1a2f6d256
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-24 14:52:13 -0300

    Update Day15-LinkedList challange

--- 30 Days of Code/Day15-LinkedList.cs
+++ 30 Days of Code/Day15-LinkedList.cs
@@ -1,48 +1,55 @@
 using System;
-class Node {
-	public int data;
-	public Node next;
-    public Node(int d){
-        data=d;
-        next=null;
-    }	
+class Node
+{
+  public int data;
+  public Node next;
+  public Node(int d)
+  {
+    data = d;
+    next = null;
+  }
 }
 
-class Solution {
+class Solution
+{
 
-	public static  Node insert(Node head,int data) {
+  public static Node insert(Node head, int data)
+  {
 
-      if (head == null) 
-          head = new Node(data);
-      else {
-          Node addNode = new Node(data);
-          Node currentNode = head;
-          
-          while(currentNode.next != null) 
-              currentNode = currentNode.next;
+    if (head == null)
+      head = new Node(data);
+    else
+    {
+      Node addNode = new Node(data);
+      Node currentNode = head;
 
-          currentNode.next = addNode;
-	  }
-      return head;
-  	}
+      while (currentNode.next != null)
+        currentNode = currentNode.next;
 
-	public static void display(Node head) {
-		Node start=head;
-		while(start!=null)
-		{
-			Console.Write(start.data+" ");
-			start=start.next;
-		}
-	}
+      currentNode.next = addNode;
+    }
+    return head;
+  }
 
-  	static void Main(String[] args) {
-      	Node head=null;
-		int T=Int32.Parse(Console.ReadLine());
-		while(T-->0){
-			int data=Int32.Parse(Console.ReadLine());
-			head=insert(head,data);
-		}
-      	display(head);
-	}
+  public static void display(Node head)
+  {
+    Node start = head;
+    while (start != null)
+    {
+      Console.Write(start.data + " ");
+      start = start.next;
+    }
+  }
+
+  static void Main(String[] args)
+  {
+    Node head = null;
+    int T = Int32.Parse(Console.ReadLine());
+    while (T-- > 0)
+    {
+      int data = Int32.Parse(Console.ReadLine());
+      head = insert(head, data);
+    }
+    display(head);
+  }
 }
-	
