a03742647ad062a622c4ab68d2a23fec6de1e1a7
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-30 17:24:47 -0300

    Resolve Day21-Generics challange

---
+++ 30 Days of Code/Day21-Generics.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Printer
+{
+
+	/**
+	*    Name: PrintArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+    // Write your code here
+
+    public static void PrintArray<T>(T[] arr){
+        
+        foreach(T element in arr){
+            Console.WriteLine(element);
+        }
+    }
+
+
+    static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+		
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
