53a85f54ed5bc6a8e875d9500cf8bd752d249ec0
Douglas Marcelino <douglas_champion@hotmail.com>
2020-09-25 23:30:16 -0300

    Resolve CatsAndAMouse problem

---
+++ Problem Solving/CatsAndAMouse.cs
@@ -0,0 +1,49 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the catAndMouse function below.
+    static string catAndMouse(int x, int y, int z) {
+        if(Math.Abs(x - z) < Math.Abs(y - z))
+            return "Cat A";
+        else if(Math.Abs(x - z) > Math.Abs(y - z))
+            return "Cat B";
+        else
+            return "Mouse C";
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            string[] xyz = Console.ReadLine().Split(' ');
+
+            int x = Convert.ToInt32(xyz[0]);
+
+            int y = Convert.ToInt32(xyz[1]);
+
+            int z = Convert.ToInt32(xyz[2]);
+
+            string result = catAndMouse(x, y, z);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
